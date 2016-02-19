2a19b6aba1e33962ad06129d33af817d2f142b5a
jmona789 <jmona789@gmail.com>
2016-02-19 17:37:45 -0500

    added handlebars files for home page, set up home route and added require for body parser

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
 var Sequelize = require("sequelize");
 
 var sequelize = new Sequelize("validationDatabase", "root");
@@ -7,6 +8,20 @@ var sequelize = new Sequelize("validationDatabase", "root");
 var PORT = process.env.NODE_ENV || 9001;
 
 var app = express();
+
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
+
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.get("/", function (req, res) {
+  res.render("home");
+});
 
 sequelize.sync().then(function () {
   app.listen(PORT, function () {
