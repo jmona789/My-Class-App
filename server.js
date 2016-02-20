604c4a8f2f6c3ed276d0079da3b6f895d1336c45
jmona789 <jmona789@gmail.com>
2016-02-20 10:00:16 -0500

    styling and added public folder for serving static content

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//defining vars for packages and port
 var express = require("express");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
@@ -9,20 +10,28 @@ var PORT = process.env.NODE_ENV || 9001;
 
 var app = express();
 
+//Serve static content for the app from the "public" directory in the application directory.
+app.use("/static", express.static("public"));
+
+//set default layout for handlebar
 app.engine("handlebars", expressHandlebars({
   defaultLayout: "main"
 }));
 
+//set handlevars and view engine
 app.set("view engine", "handlebars");
 
+//body parser options
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 
+//home route
 app.get("/", function (req, res) {
   res.render("home");
 });
 
+//sync sequelize and tell server to listen
 sequelize.sync().then(function () {
   app.listen(PORT, function () {
     console.log("LISTENING!");
